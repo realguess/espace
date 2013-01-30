542898e1c78bb29f7591c84dff2e1be574fd66ed
Chao Huang <chao@realguess.net>
2013-01-30 15:59:17 -0500

    Add previous day chart

--- ping.bash
+++ ping.bash
@@ -3,9 +3,18 @@
 # Ping, save and push.
 ##
 
+LAST=$(date '+%Y%m%d')
+
 while [ 1 ]; do
   # Set date
   DATE=`date '+%Y%m%d'`
+
+  # Previous date
+  if [ ${DATE} != ${LAST} ]; then
+    cp plot.png plot-last.png    
+    git add plot-last.png
+    LAST=${DATE}
+  fi
 
   # Ping google.com
   ping -i 9 -q -c 100 74.125.226.229 | awk '/^[0-9]/ { print time,$4 }' "time=$(date '+%s %Y-%m-%d %H:%M')" >> data/${DATE}.csv
